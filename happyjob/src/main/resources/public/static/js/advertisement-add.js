3ce53a9378e46c83ea1a8d40049e2ed4c69e8053
zhuoyue9527 <zhuoyue9527@163.com>
2019-01-06 10:16:45 +0800

    广告添加

--- happyjob/src/main/resources/public/static/js/advertisement-add.js
+++ happyjob/src/main/resources/public/static/js/advertisement-add.js
@@ -4,6 +4,24 @@ $(".datepicker").datepicker({
     autoclose: true,//选中之后自动隐藏日期选择框
     format: "yyyy-mm-dd"//日期格式，详见 http://bootstrap-datepicker.readthedocs.org/en/release/options.html#format
 });
+
+$('#upimg').on('change',function(){                                                 //选中图片后展示在页面
+    var filePath = $(this)[0].files[0].name //获取到input的value，里面是文件的路径 
+   console.log(filePath) //1.png 
+   fileFormat = filePath.split('.')[1].toLowerCase() 
+   console.log(fileFormat) //png
+   src = window.URL.createObjectURL(this.files[0]) //转成可以在本地预览的格式 
+   console.log(src) //blob:null/11ea5a2d-7270-4035-b5c4-4e50c5c061e7
+
+  // 检查是否是图片 
+  if( !fileFormat.match(/png|jpg|jpeg/) ) { 
+      alert('上传错误,文件格式必须为：png/jpg/jpeg')
+     return 
+  } 
+
+$('#imgContent').attr('src',src)
+
+})
 // 禁用
 $(document).on("click",".forbidden",function(){
     swal({
@@ -120,70 +138,6 @@ $(document).on("click",".cat",function(){
 })
 
 
-var listParams = {
-    phoneNo:"",
-    resource:"",
-    startTime:"",
-    endTime:"",
-    blackOn:"",
-    userType:"",
-    currentPage:1,
-    showCount:10
-}
-var list= [];
-var totalPage=1;
-
-fetchList();
-/**
- *
-applyTime (integer, optional): 认证申请时间 ,
-approveNum (integer, optional): 认证申请次数 ,
-approveState (integer, optional): 认证状态（0、未申请认证，1、认证通过，2、认证不通过，3、认证待审核） ,
-blackOn (integer, optional): 是否被禁用 ,
-bornYear (integer, optional): 出生年份（时间戳s），计算年龄 ,
-comName (string, optional): 公司名称 ,
-createTime (integer, optional): 创建时间 ,
-gender (integer, optional): 性别（1、男，2、女） ,
-headerPic (string, optional): 头像 ,
-hpCompanyId (integer, optional): 企业id ,
-hpUserId (integer, optional): 用户ID ,
-idBackPic (string, optional): 身份证反面图片 ,
-idFrontPic (string, optional): 身份证正面图片 ,
-idNum (string, optional): 身份证号 ,
-idPersonPic (string, optional): 身份证手持图片 ,
-inviteUserId (integer, optional): 邀请人ID ,
-loginIp (string, optional): 登录ip ,
-loginTime (integer, optional): 最近登录时间 ,
-password (string, optional): 密码 ,
-phoneNo (string, optional): 手机号码 ,
-realName (string, optional): 真实姓名 ,
-registResource (integer, optional): 注册来源（1、APP，2、小程序，3、微信，4、门店，5、邀请注册） ,
-salt (string, optional): 密码加盐 ,
-shareToken (string, optional): 分享身份识别码 ,
-tokenTime (integer, optional): token有效时间 ,
-userMoney (number, optional): 账户余额 ,
-userName (string, optional): 用户名（数字、字母、下划线） ,
-userToken (string, optional): 登录token ,
-userType (integer, optional): 账号类型（1、超级管理员，2、求职者） ,
-vipOn (integer, optional): 是否会员
- */
-
- /**
-  * 
-blackOn (integer, optional): 用户是否被禁用 ,
-currentPage (integer, optional): 当前页 ,
-currentResult (integer, optional): 当前记录起始索引 ,
-endTime (integer, optional): 注册时间截止 ,
-entityOrField (boolean, optional): true:需要分页的地方，传入的参数就是Page实体；false:需要分页的地方，传入的参数所代表的实体拥有Page属性 ,
-isPage (integer, optional): 是否分页:自定义分页时使用，插件分页不需要赋值 ,
-phoneNo (string, optional): 用户手机号 ,
-resource (integer, optional): 用户注册来源 ,
-showCount (integer, optional): 每页显示记录数 ,
-startTime (integer, optional): 注册时间开始 ,
-totalPage (integer, optional): 总页数 ,
-totalResult (integer, optional): 总记录数 ,
-userType (integer, optional): 用户是否被禁用
-  */
 
 // 获取table数据
 function fetchList(){
@@ -209,42 +163,7 @@ function postAuth(data){
         }
     })
 }
-// 添加table数据
-function addTableList(list){
-    var $table = $("#userList");
-    var $tBody = $table.find("tbody");
-    var templeteTr="";
-    list.forEach(function(item){
-        templeteTr+='\
-        <tr \
-            data-user-id="'+ item.hpUserId +'" \
-            data-iphone="'+ item.phoneNo +'" \
-            data-name="'+ item.realName +'" \
-            data-born-year="'+ item.bornYear +'" \
-            data-id-num="'+ item.idNum +'" \
-            data-create-time="'+ timestampToTime(item.createTime) +'" \
-            data-comname="'+ (item.comName || "") +'" \
-            data-approve-state="'+ approveState(item.approveState)  +'" \
-            data-id-front-pic="'+ item.idFrontPic +'" \
-            data-id-back-pic="'+ item.idBackPic +'" >\
-            <th>'+ item.phoneNo +'</th>\
-            <th>'+ item.realName +'</th>\
-            <th>'+ gender(item.gender) +'</th>\
-            <th>'+ bornYear(item.bornYear) +'</th>\
-            <th>'+ userType(item.userType) +'</th>\
-            <th>'+ timestampToTime(item.createTime) +'</th>\
-            <th>'+ registResource(item.registResource) +'</th>\
-            <th>'+ approveState(item.approveState) +'</th>\
-            <th>\
-                <button type="button" class="btn btn-default btn-sm cat">查看</button>\
-                <button type="button" class="btn btn-primary btn-sm auth">认证</button>\
-                <button type="button" class="btn btn-danger btn-sm forbidden">禁用</button>\
-            </th>\
-        </tr>';
-                // <button type="button" class="btn btn-danger btn-sm restart">复用</button>\
-    })
-    $tBody.html(templeteTr)    
-}
+
 // 判断性别
 function gender(gender){    
     return gender==1?"男":"女"
