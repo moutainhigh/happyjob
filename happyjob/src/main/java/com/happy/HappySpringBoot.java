dd5e680adf8afa962fb746a15a7922e07332051e
sunning <sunning@auto-fis.com>
2019-01-07 19:42:28 +0800

    SmsUtil 自动注入
    SmsUtil 自动注入

--- happyjob/src/main/java/com/happy/HappySpringBoot.java
+++ happyjob/src/main/java/com/happy/HappySpringBoot.java
@@ -3,7 +3,9 @@ package com.happy;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -13,15 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.happy.util.ServiceConfig;
 import com.happy.util.Util;
+import com.happy.util.sms.SmsUtil;
 
 @SpringBootApplication
-@MapperScan({ "com.happy.sqlMapper", "com.happy.sqlExMapper" })
+@MapperScan({"com.happy.sqlMapper", "com.happy.sqlExMapper"})
 @RestController
-public class HappySpringBoot extends SpringBootServletInitializer implements EnvironmentAware {
-    
+public class HappySpringBoot extends SpringBootServletInitializer implements EnvironmentAware, CommandLineRunner {
+
     private static final Logger logger = LoggerFactory.getLogger(HappySpringBoot.class);
-    
-    
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return configureApplication(builder);
@@ -29,35 +31,43 @@ public class HappySpringBoot extends SpringBootServletInitializer implements Env
 
     public static void main(String[] args) {
         configureApplication(new SpringApplicationBuilder()).run(args);
-    
+
     }
 
     private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
         return builder.sources(HappySpringBoot.class).bannerMode(Banner.Mode.OFF);
     }
-    
-    
 
     @Override
     public void setEnvironment(Environment environment) {
-        
-        //可使用反射替代
+
+        // 可使用反射替代
         String upload_base_path = environment.getProperty("serviceConfig.filePath.upload_base_path");// 根据自己定义的获取文件上传地址
         String upload_base_url = environment.getProperty("serviceConfig.filePath.upload_base_url");// 根据自己定义的获取文件上传后访问域名
         String swaggerEnabled = environment.getProperty("serviceConfig.swagger.enabled");// 根据自己定义的获取swagger接口是否开放
-        logger.info("upload_base_path:{}",upload_base_path);
-        logger.info("upload_base_url:{}",upload_base_url);
-        logger.info("swaggerEnabled:{}",swaggerEnabled);
-        if(!Util.isEmpty(upload_base_path)) {
+        logger.info("upload_base_path:{}", upload_base_path);
+        logger.info("upload_base_url:{}", upload_base_url);
+        logger.info("swaggerEnabled:{}", swaggerEnabled);
+        if (!Util.isEmpty(upload_base_path)) {
             ServiceConfig.setUploadBasePath(upload_base_path);
         }
-        if(!Util.isEmpty(upload_base_url)) {
+        if (!Util.isEmpty(upload_base_url)) {
             ServiceConfig.setUploadBaseUrl(upload_base_url);
         }
-        if("true".equals(swaggerEnabled)) {
+        if ("true".equals(swaggerEnabled)) {
             ServiceConfig.setSwaggerEnabled(true);
         }
-         
+
+    }
+
+    /*
+     * 启动后可以做一些动作
+     */
+    @Override
+    public void run(String... args) throws Exception {
+
+        logger.debug("==========启动成功!===========");
+
     }
 
 }
--- happyjob/src/main/java/com/happy/util/sms/SmsUtil.java
+++ happyjob/src/main/java/com/happy/util/sms/SmsUtil.java
@@ -7,6 +7,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import com.alibaba.fastjson.JSON;
@@ -20,18 +22,33 @@ import okhttp3.Response;
 /**
  * 短信发送工具
  */
+@Configuration
 @Component
 public class SmsUtil {
 
+    private static String account;// N6000001" 用户在253云通讯平台上申请的API账号
+    private static String password;// 123456" 用户在253云通讯平台上申请的API账号对应的API密钥
+    private static String smsUrl; // http://xxx/msg/send/json 或者 https://xxx/msg/send/json
+    private static String smsSign; // 在zz.253.com 后台设置签名管理
+
     @Autowired
-    private  static Zz253Property zz253Property;
+    private Zz253Property property;
+
+    @Bean
+    public Object virtualObject() {
+        account = property.getAccount();
+        password = property.getPassword();
+        smsUrl = property.getSmsUrl();
+        smsSign = property.getSmsSign();
+        return new Object();
+    }
 
     private static final Logger logger = LoggerFactory.getLogger(SmsUtil.class);
 
     private static final OkHttpClient client = new OkHttpClient();
 
     public static final MediaType MEDIA_TYPE_JSON = MediaType.parse("application/json; charset=utf-8");
-    
+
     public static final String MSG_MODEL = "尊敬的用户： 您的验证码为${msgCode}，如非本人操作，请忽略此短信。";
 
     /**
@@ -39,20 +56,20 @@ public class SmsUtil {
      * @param phoneNum 手机号码
      * @return true 表示提交发送指令成功，false 表示提交指令失败
      */
-    public static boolean sendSms(String msg, String phoneNum) {
 
+    public static boolean sendSms(String msg, String phoneNum) {
         try {
 
             Zz253Request zz253Req = new Zz253Request();
-            zz253Req.setAccount(zz253Property.getAccount());
-            zz253Req.setPassword(zz253Property.getPassword());
-            //拼接签名
-            zz253Req.setMsg(StringUtils.appendIfMissing(zz253Property.getSmsSign(), msg));
+            zz253Req.setAccount(account);
+            zz253Req.setPassword(password);
+            // 拼接签名
+            zz253Req.setMsg(StringUtils.appendIfMissing(smsSign, msg));
             zz253Req.setPhone(phoneNum);
             String postBody = JSON.toJSONString(zz253Req);
 
-            Request request = new Request.Builder().url(zz253Property.getSmsUrl())
-                .post(RequestBody.create(MEDIA_TYPE_JSON, postBody)).build();
+            Request request =
+                new Request.Builder().url(smsUrl).post(RequestBody.create(MEDIA_TYPE_JSON, postBody)).build();
 
             Response response = client.newCall(request).execute();
 
